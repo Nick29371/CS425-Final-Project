7ae7433565cd8c0e5df263195a7c2c8aa77f2435
iitedilsonalberto <113647010+iitedilsonalberto@users.noreply.github.com>
2024-09-22 08:43:27 -0500

    Update hw2.sql

--- hw2.sql
+++ hw2.sql
@@ -30,3 +30,16 @@ SELECT *
 FROM hw2.person
 WHERE `BirthCountry,` IN ('GAB,', 'EGY,');
 
+-- EXERCISE 7
+SELECT 
+    p.Points, 
+    t.Name AS TeamFullName, 
+    c.BioID AS CoachBioID
+FROM 
+    hw2.player p
+JOIN 
+    hw2.team t ON p.TmID = t.TmID
+JOIN 
+    hw2.coach c ON t.TmID = c.TmID
+WHERE 
+    p.BioID = 'Jamesle01';
