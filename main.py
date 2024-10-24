75debd82fc639d40658d8e7200d8d5d4ba9e6568
Nick29371 <144848194+Nick29371@users.noreply.github.com>
2024-10-23 22:33:51 -0500

    commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi")
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "graduation",
+    database = 'project'
+)
+
+
+mycursor = db.cursor()
+
+mycursor.execute("SELECT * FROM genre")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
